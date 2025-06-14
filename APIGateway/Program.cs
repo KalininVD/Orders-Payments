a53a55080fbdf640915c499d59e1f2eb873243da
Vlad_K <kalinin.vd@yandex.ru>
2025-06-15 01:13:53 +0300

    Changed docker-compose strategy

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,26 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var reverseProxySection = builder.Configuration.GetSection("ReverseProxy");
-
-void ReplaceEnvVarsInSection(IConfigurationSection section)
-{
-    foreach (var child in section.GetChildren())
-    {
-        if (child.Value != null)
-        {
-            var newValue = Environment.ExpandEnvironmentVariables(child.Value);
-            builder.Configuration[$"{child.Path}"] = newValue;
-        }
-        else
-        {
-            ReplaceEnvVarsInSection(child);
-        }
-    }
-}
-
-ReplaceEnvVarsInSection(reverseProxySection);
-
-builder.Services.AddReverseProxy().LoadFromConfig(reverseProxySection);
+builder.Services.AddReverseProxy().LoadFromConfig(
+    builder.Configuration.GetSection("ReverseProxy")
+);
 
 var app = builder.Build();
 
