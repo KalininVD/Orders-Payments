58ca6669bc863cf5e4ce36683a9f263672fd5089
Vlad_K <kalinin.vd@yandex.ru>
2025-06-15 15:36:23 +0300

    Removed duplicate line

--- OrdersService/OrdersService.Web/Program.cs
+++ OrdersService/OrdersService.Web/Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
