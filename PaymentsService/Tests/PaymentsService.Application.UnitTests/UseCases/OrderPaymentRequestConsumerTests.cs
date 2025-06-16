4c21af332606ecbaca6ed171949bb0f5dc5c92f2
Vlad_K <kalinin.vd@yandex.ru>
2025-06-17 01:05:07 +0300

    Fixed OrderPaymentRequestConsumer Test

--- PaymentsService/Tests/PaymentsService.Application.UnitTests/UseCases/OrderPaymentRequestConsumerTests.cs
+++ PaymentsService/Tests/PaymentsService.Application.UnitTests/UseCases/OrderPaymentRequestConsumerTests.cs
@@ -15,6 +15,7 @@ public class OrderPaymentRequestConsumerTests
     private readonly Mock<IAccountRepository> _accountRepositoryMock;
     private readonly Mock<IUnitOfWork> _unitOfWorkMock;
     private readonly Mock<IPublishEndpoint> _publishEndpointMock;
+    private readonly Mock<IDelayProvider> _delayProviderMock;
     private readonly Mock<ILogger<OrderPaymentRequestConsumer>> _loggerMock;
     private readonly OrderPaymentRequestConsumer _consumer;
 
@@ -23,12 +24,14 @@ public class OrderPaymentRequestConsumerTests
         _accountRepositoryMock = new Mock<IAccountRepository>();
         _unitOfWorkMock = new Mock<IUnitOfWork>();
         _publishEndpointMock = new Mock<IPublishEndpoint>();
+        _delayProviderMock = new Mock<IDelayProvider>();
         _loggerMock = new Mock<ILogger<OrderPaymentRequestConsumer>>();
 
         _consumer = new OrderPaymentRequestConsumer(
             _accountRepositoryMock.Object,
             _unitOfWorkMock.Object,
             _publishEndpointMock.Object,
+            _delayProviderMock.Object,
             _loggerMock.Object);
     }
 
